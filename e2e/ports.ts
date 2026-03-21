ad54579d763779a87bc0ce9878cd60b12d9988a7
Paul DiLoreto <soodohh@pm.me>
2026-03-21 02:26:10 -0700

    chore: install Playwright and better-sqlite3, create e2e directory structure

---
+++ e2e/ports.ts
@@ -0,0 +1,16 @@
+// Fixed ports for fake servers (started once in global setup)
+const PORTS = {
+  QBITTORRENT: 19_001,
+  TRANSMISSION: 19_002,
+  DELUGE: 19_003,
+  RTORRENT: 19_004,
+  SABNZBD: 19_005,
+  NZBGET: 19_006,
+  NEWZNAB: 19_007,
+  PROWLARR: 19_008,
+  HARDCOVER: 19_009,
+  // App server ports start at 19100, incremented per worker
+  APP_BASE: 19_100,
+} as const;
+
+export default PORTS;
