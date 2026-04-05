ebb46de2ccdda7411144ebd7c5bca547cf3d7ab1
Paul DiLoreto <soodohh@pm.me>
2026-04-05 14:27:53 -0700

    feat: configure better-auth admin + genericOAuth plugins with databaseHooks

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,8 @@
+import { adminClient, genericOAuthClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
-export const authClient = createAuthClient();
+export const authClient = createAuthClient({
+	plugins: [adminClient(), genericOAuthClient()],
+});
 
 export const { signIn, signUp, signOut, useSession } = authClient;
