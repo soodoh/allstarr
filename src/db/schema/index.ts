447b15b91b7272f9a65279f7085804a12b284456
Paul DiLoreto <soodohh@pm.me>
2026-04-05 14:24:17 -0700

    feat: add OIDC providers schema

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -22,6 +22,7 @@ export * from "./movie-download-profiles";
 export * from "./movie-files";
 export * from "./movie-import-list-exclusions";
 export * from "./movies";
+export * from "./oidc-providers";
 export * from "./profile-custom-formats";
 export * from "./scheduled-tasks";
 export * from "./series";
