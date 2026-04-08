a5671d345e7f616481793e953d9958cd6383307e
Paul DiLoreto <soodohh@pm.me>
2026-04-07 23:07:09 -0700

    chore: cleanup docs

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,10 +1,8 @@
-import { RuleConfigSeverity } from "@commitlint/types";
 import type { UserConfig } from "@commitlint/types";
 
 const Configuration: UserConfig = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "scope-empty": [RuleConfigSeverity.Error, "never"],
   },
 };
 
