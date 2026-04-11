694921cf84069126b316fa0b19083b0dbad79977
Paul DiLoreto <soodohh@pm.me>
2026-04-10 20:02:30 -0700

    test(episode-profiles): use shared runMutation helper

--- src/hooks/mutations/episode-profiles.browser.test.ts
+++ src/hooks/mutations/episode-profiles.browser.test.ts
@@ -1,5 +1,5 @@
 import { QueryClient } from "@tanstack/react-query";
-import { renderHook } from "src/test/render";
+import { runMutation } from "src/test/mutations";
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 
 const {
@@ -46,25 +46,6 @@ import {
 	useMonitorEpisodeProfile,
 	useUnmonitorEpisodeProfile,
 } from "./episode-profiles";
-
-type HookRunner = () => {
-	mutateAsync: (variables: unknown) => Promise<unknown>;
-};
-
-async function runMutation(
-	useHook: HookRunner,
-	variables: unknown,
-	swallowError = false,
-) {
-	const { result } = await renderHook(() => useHook());
-
-	const promise = result.current.mutateAsync(variables as never);
-	if (swallowError) {
-		await promise.catch(() => {});
-		return;
-	}
-	await promise;
-}
 
 describe("mutations/episode-profiles", () => {
 	beforeEach(() => {
