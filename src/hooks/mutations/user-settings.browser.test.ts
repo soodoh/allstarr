27ae9d065b28a7c039f24e799cfe08275ff058fe
Paul DiLoreto <soodohh@pm.me>
2026-04-10 20:03:03 -0700

    test(user-settings): use shared runMutation helper

--- src/hooks/mutations/user-settings.browser.test.ts
+++ src/hooks/mutations/user-settings.browser.test.ts
@@ -1,5 +1,5 @@
 import { QueryClient } from "@tanstack/react-query";
-import { renderHook } from "src/test/render";
+import { runMutation } from "src/test/mutations";
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 
 const {
@@ -26,25 +26,6 @@ vi.mock("src/server/user-settings", () => ({
 import { queryKeys } from "src/lib/query-keys";
 
 import { useResetColumnSettings, useUpsertUserSettings } from "./user-settings";
-
-type HookRunner = () => {
-	mutateAsync: (variables: unknown) => Promise<unknown>;
-};
-
-async function runMutation(
-	useHook: HookRunner,
-	variables: unknown,
-	swallowError = false,
-) {
-	const { result } = await renderHook(() => useHook());
-
-	const promise = result.current.mutateAsync(variables as never);
-	if (swallowError) {
-		await promise.catch(() => {});
-		return;
-	}
-	await promise;
-}
 
 describe("mutations/user-settings", () => {
 	beforeEach(() => {
