398238aa37fa6ba6bfbc193f26aa05fe5495916b
Paul DiLoreto <soodohh@pm.me>
2026-04-10 20:02:09 -0700

    test(download-clients): use shared runMutation helper

--- src/hooks/mutations/download-clients.browser.test.ts
+++ src/hooks/mutations/download-clients.browser.test.ts
@@ -1,5 +1,5 @@
 import { QueryClient } from "@tanstack/react-query";
-import { renderHook } from "src/test/render";
+import { runMutation } from "src/test/mutations";
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 
 const {
@@ -41,25 +41,6 @@ import {
 	useDeleteDownloadClient,
 	useUpdateDownloadClient,
 } from "./download-clients";
-
-type HookRunner = () => {
-	mutateAsync: (variables: unknown) => Promise<unknown>;
-};
-
-async function runMutation(
-	useHook: HookRunner,
-	variables: unknown,
-	swallowError = false,
-) {
-	const { result } = await renderHook(() => useHook());
-
-	const promise = result.current.mutateAsync(variables as never);
-	if (swallowError) {
-		await promise.catch(() => {});
-		return;
-	}
-	await promise;
-}
 
 describe("mutations/download-clients", () => {
 	beforeEach(() => {
