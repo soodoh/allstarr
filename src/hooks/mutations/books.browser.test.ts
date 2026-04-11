0dfdcbb49d4677e4c1a278f5b4334ed7010ee5ca
Paul DiLoreto <soodohh@pm.me>
2026-04-10 20:00:22 -0700

    test(books): use shared runMutation helper

--- src/hooks/mutations/books.browser.test.ts
+++ src/hooks/mutations/books.browser.test.ts
@@ -1,5 +1,5 @@
 import { QueryClient } from "@tanstack/react-query";
-import { renderHook } from "src/test/render";
+import { runMutation } from "src/test/mutations";
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 
 const {
@@ -66,25 +66,6 @@ import {
 	useUnmonitorBookProfile,
 	useUpdateBook,
 } from "./books";
-
-type HookRunner = () => {
-	mutateAsync: (variables: unknown) => Promise<unknown>;
-};
-
-async function runMutation(
-	useHook: HookRunner,
-	variables: unknown,
-	swallowError = false,
-) {
-	const { result } = await renderHook(() => useHook());
-
-	const promise = result.current.mutateAsync(variables as never);
-	if (swallowError) {
-		await promise.catch(() => {});
-		return;
-	}
-	await promise;
-}
 
 describe("mutations/books", () => {
 	beforeEach(() => {
